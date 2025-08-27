242c010382c7ac3a314f468a184180ba63db74b8
leo <leo40412@gmail.com>
2025-08-27 21:38:55 +0800

    完成SQL寫入語法

--- pm25.py
+++ pm25.py
@@ -26,7 +26,11 @@ def open_db():
 
     try:
         conn = pymysql.connect(
-            host="127.0.0.1", user="root", password="", port=3307, database="demo"
+            host="mysql-3597b72c-ouhanxiang484-pm25project.i.aivencloud.com",
+            user="avnadmin",
+            password="AVNS_niEr1CxTASTTpMrZ-5P",
+            port=12799,
+            database="defaultdb"
         )
 
         # print(conn)
@@ -62,6 +66,32 @@ def write_to_sql():
     except Exception as e:
         print(e)
 
+def get_data_from_mysql():
+
+    try:
+        open_db()
+        # sqlstr="select max(datacreationdate) from pm25;"
+        # cursor.execute(sqlstr)
+        # max_data=cursor.fetchone()
+        # print(max_data)
+
+        sqlstr="select site,county,pm25,datacreationdate,itemunit " \
+        "from pm25 " \
+        "where datacreationdate=(select max(datacreationdate) from pm25);"
+        cursor.execute(sqlstr)
+        datas=cursor.fetchall()
+        # 去掉ID
+        # datas=[data[1:] for data in datas]
+
+        return datas
+    except Exception as e:
+        print(e)
+    finally:
+        close_db()
+    
+    return None
+
+# print(get_data_from_mysql())
 
 # open_db()
 # write_to_sql()
